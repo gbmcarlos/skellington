aa65373100eb4a05b826e4166c918253eb525804
Carlos Garcia-Berro Molero <gbmcarlos@gmail.com>
2020-05-29 19:48:26 +0400

    fix index.php

--- src/public/index.php
+++ src/public/index.php
@@ -2,7 +2,7 @@
 
 require '/opt/vendor/autoload.php';
 
-$app = require getenv('LAMBDA_TASK_ROOT') . '/src/bootstrap/app.php';
+$app = require '/var/task/src/bootstrap/app.php';
 
 $bootstrap = new \Toolkit\Libraries\Foundation\Lumen\Bootstrap($app);
 
